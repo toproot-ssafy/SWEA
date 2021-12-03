ce2b4833878267b01d9022ed06be0874923c338b
toproot <toproot7913@gmail.com>
2021-12-03 20:29:52 +0900

    2056 년월일 clear

--- python_D1/2056. 연월일 달력.py
+++ python_D1/2056. 연월일 달력.py
@@ -2,10 +2,22 @@ import sys
 sys.stdin = open("2056.txt", "r")
 
 N = int(input())
-for n in range(N) :
-  numbers = list(map(int, input()))
-  year=0
-  month=0
-  date=0
-  for number in numbers :
+for n in range(N):
+  numbers = str(input())
+  year = numbers[:4]
+  month = numbers[4:6]
+  date = numbers[6:]
+
+  result = -1
+
+  if (0 < int(month) < 13):
+    if int(month) in [2] and (0 < int(date) < 29):
+      result = year + '/' + month + '/' + date
+    if int(month) in [1, 3, 5, 7, 8, 10, 12] and (0 < int(date) < 32):
+      result = year + '/' + month + '/' + date
+    if int(month) in [4, 6, 9, 11] and (0 < int(date) < 31):
+      result = year + '/' + month + '/' + date
+
+  print('#{} {}'.format(n+1, result))
+
 

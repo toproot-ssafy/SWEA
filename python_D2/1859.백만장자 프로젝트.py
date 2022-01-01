1cf77495d3da9195ce256dbe2197fa70c6c7386f
toproot <toproot7913@gmail.com>
2022-01-01 23:23:45 +0900

    1859.백만장자 프로젝트

--- python_D2/1859.백만장자 프로젝트.py
+++ python_D2/1859.백만장자 프로젝트.py
@@ -2,9 +2,23 @@ import sys
 sys.stdin = open('1859.txt','r')
 
 TC = int(input())
-TC = 2
+
+def calPrice(Days):
+  MaxNum = Day[-1] # 맨 뒤의 숫자부터 크기 비교
+  Day.reverse() # 역순으로 바꾼 후 탐색
+  Value = 0 # 시세차익
+  for i in Day:
+    if i > MaxNum:
+      MaxNum = i
+    else:
+      Value += MaxNum - i
+  return Value
+
 for tc in range(TC):
   N = int(input())
   Day = list(map(int, input().split()))
-  print(N, Day)
+  Result = 0
+  Result += calPrice(Day)
+
+  print('#{}'.format(tc+1),Result)
 

a987768e6821ef08c9667afcff7f2e6328fcc051
toproot <toproot7913@gmail.com>
2022-01-03 23:26:36 +0900

    2007. 패턴마디의 길이

---
+++ python_D2/2007. 패턴 마디의 길이.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('2007.txt', 'r')
+
+TC = int(input())
+
+for tc in range(1, TC+1):
+  Pattern = input()
+  patterWord = ''
+  for i in range(1, 11):
+    word = Pattern[0:i] # 0
+    if word == Pattern[i : i+len(word)]:
+

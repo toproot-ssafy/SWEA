e2951ed7cc07ed1420d3a1e7a2c7c2bce4fc7aa6
toproot <toproot7913@gmail.com>
2022-01-04 21:55:18 +0900

    2005.파스칼의 삼각형

---
+++ python_D2/2005. 파스칼의 삼각형.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('2005.txt', 'r')
+
+TC = int(input())
+
+for tc in range(1, TC+1):
+  N = int(input())
+  for i in range(N):
+    numList = list(map(int, range(i)))
+    for j in range(i):
+      if

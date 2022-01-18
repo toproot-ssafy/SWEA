1f8088ab7df9b7289bf74cd8bbe58bc600e24c2c
toproot <toproot7913@gmail.com>
2022-01-18 23:36:01 +0900

    2007. 패턴 마디의 길이

--- python_D2/2007. 패턴 마디의 길이.py
+++ python_D2/2007. 패턴 마디의 길이.py
@@ -5,7 +5,4 @@ TC = int(input())
 
 for tc in range(1, TC+1):
   Pattern = input()
-  patterWord = ''
-  for i in range(1, 11):
-    word = Pattern[0:i] # 0
-    if word == Pattern[i : i+len(word)]:
+  print(Pattern)
